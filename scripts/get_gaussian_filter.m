f85edc2b84cd5ea589fb67fd19dfe35ed317d462
gabrielriqu3ti <gabrielhriqueti@gmail.com>
2021-05-28 13:56:30 -0300

    Renomear size para filter_size que não é uma keyword do Matlab

--- scripts/get_gaussian_filter.m
+++ scripts/get_gaussian_filter.m
@@ -1,10 +1,10 @@
-function h = get_gaussian_filter(size, std_dev)
+function h = get_gaussian_filter(filter_size, std_dev)
 %get_gaussian_filter - Gera um filtro gaussiano não-causal representado por um 
 % vetor linha. O desvio-padrão é igual à metado do (tamanho do 
 % filtro - 1) e o ganho vale 1.
 % 
 % Entradas:
-%       size             : número ímpar positivo
+%       filter_size      : número ímpar positivo
 %                          tamanho n do filtro n x 1
 %       std_dev          : número positivo
 %                          desvio-padrão do filtro
@@ -14,9 +14,9 @@ function h = get_gaussian_filter(size, std_dev)
 
     gauss = @(x, mu, sigma) exp(-((x - mu)./sigma).^2./2);
     
-    if (mod(size, 2) == 1) && (size > 1)
-        size_minus_1_half = (abs(size) - 1) / 2;
-        n = -size_minus_1_half:1:size_minus_1_half;
+    if (mod(filter_size, 2) == 1) && (filter_size > 1)
+        filter_size_minus_1_half = (abs(filter_size) - 1) / 2;
+        n = -filter_size_minus_1_half:1:filter_size_minus_1_half;
         h = gauss(n, 0, std_dev);
         h = h ./ sum(h);
     else
